20392864eb8ed88882526cf430c89b622b5e4948
Ribal Aladeeb <aladeeb.r@hotmail.com>
2022-02-06 21:53:17 -0500

    Solve problem 6
    https://leetcode.com/problems/zigzag-conversion/

---
+++ 6/6.py
@@ -0,0 +1,30 @@
+
+'''
+https://leetcode.com/problems/zigzag-conversion/
+'''
+
+
+def solution(s: str, numRows: int):
+    word = s
+
+    rows = [""] * numRows
+
+    i = 0
+
+    while i < len(word):
+        for forward_iter in range(numRows):
+            if i >= len(word):
+                break
+            rows[forward_iter] += word[i]
+            i += 1
+
+        for backward_iter in reversed(range(1, numRows-1)):
+            if i >= len(word):
+                break
+            rows[backward_iter] += word[i]
+            i += 1
+
+    return "".join(rows)
+
+
+print(solution('a', 1))
