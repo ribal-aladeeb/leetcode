4a53bdaad29e6aa587fe84c2cd68af8ace9a745a
Ribal Aladeeb <aladeeb.r@hotmail.com>
2025-09-16 14:33:11 -0400

    fix: valid sudoku reference bug

--- neetcode/arrays_and_hashing/valid_sudoku.py
+++ neetcode/arrays_and_hashing/valid_sudoku.py
@@ -5,9 +5,9 @@ import json
 class Solution:
     def isValidSudoku(board: list[list[str]]) -> bool:
         # valid_nums = [str(x) for x in range(1, 10)]
-        rows: list[set] = [set()] * 9
-        columns: list[set] = [set()] * 9
-        boxes: list[set] = [set()] * 9
+        rows: list[set] = [set() for _ in range(9)]
+        columns: list[set] = [set() for _ in range(9)]
+        boxes: list[set] = [set() for _ in range(9)]
         box_mappings: dict[tuple[str], int] = {}
 
         # box_mappings = (
@@ -34,23 +34,26 @@ class Solution:
             for i in range(i_start, i_start + 3):
                 j_start = box_index % 3 * 3
                 for j in range(j_start, j_start + 3):
-                    assert (i, j) not in box_mappings
-                    box_mappings[(i, j)] = box_index + 1
-
-        print(json.dumps({str(k): v for k, v in box_mappings.items()}, indent=2))
-        print(len(box_mappings))
+                    box_mappings[(i, j)] = box_index
 
         for i in range(9):
-            for i in range(9):
+            for j in range(9):
                 if board[i][j] == ".":
                     continue
-                number = board[i][j]
+                number = int(board[i][j])
                 box_of_interest = boxes[box_mappings[(i, j)]]
-                if (
-                    number in rows[i]
-                    or number in columns[j]
-                    or number in box_of_interest
-                ):
+                if number in rows[i]:
+                    print(f"number {number} in row {i+1}")
+                    print(f"at index ({i},{j})")
+                    print(rows[i])
+                    return False
+
+                if number in columns[j]:
+                    print(f"number {number} in column {i+1}")
+                    return False
+
+                if number in box_of_interest:
+                    print(f"number {number} in box {box_mappings[(i,j)]}")
                     return False
                 rows[i].add(number)
                 columns[j].add(number)
@@ -59,4 +62,16 @@ class Solution:
         return True
 
 
-Solution.isValidSudoku(board=[])
+x = [
+    ["1", "2", ".", ".", "3", ".", ".", ".", "."],
+    ["4", ".", ".", "5", ".", ".", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", ".", "3"],
+    ["5", ".", ".", ".", "6", ".", ".", ".", "4"],
+    [".", ".", ".", "8", ".", "3", ".", ".", "5"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", ".", ".", ".", ".", ".", "2", ".", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "8"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+]
+
+print(Solution.isValidSudoku(board=x))
