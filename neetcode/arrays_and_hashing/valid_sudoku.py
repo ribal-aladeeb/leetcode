e0a532f68b76f482fb31ec56abe196f2fc44092b
Ribal Aladeeb <aladeeb.r@hotmail.com>
2025-09-16 14:39:50 -0400

    cleanup valid_sudoku solution

--- neetcode/arrays_and_hashing/valid_sudoku.py
+++ neetcode/arrays_and_hashing/valid_sudoku.py
@@ -10,25 +10,6 @@ class Solution:
         boxes: list[set] = [set() for _ in range(9)]
         box_mappings: dict[tuple[str], int] = {}
 
-        # box_mappings = (
-        #     {(i, j): 1 for i in range(0, 3) for j in range(0, 3)}
-        #     | {(i, j): 2 for i in range(0, 3) for j in range(3, 6)}
-        #     | {(i, j): 3 for i in range(0, 3) for j in range(6, 9)}
-        #     | {(i, j): 4 for i in range(3, 6) for j in range(0, 3)}
-        #     | {(i, j): 5 for i in range(3, 6) for j in range(3, 6)}
-        #     | {(i, j): 6 for i in range(3, 6) for j in range(6, 9)}
-        #     | {(i, j): 7 for i in range(6, 9) for j in range(0, 3)}
-        #     | {(i, j): 8 for i in range(6, 9) for j in range(3, 6)}
-        #     | {(i, j): 9 for i in range(6, 9) for j in range(6, 9)}
-        # )
-
-        # box_mappings = {
-        #     (i, j): box_index + 1
-        #     for i in range(box_index // 3 * 3, box_index // 3 * 3 + 3)
-        #     for j in range(box_index % 3 * 3, box_index % 3 * 3 + 3)
-        #     for box_index in range(9)
-        # }
-
         for box_index in range(9):
             i_start = box_index // 3 * 3
             for i in range(i_start, i_start + 3):
@@ -42,23 +23,15 @@ class Solution:
                     continue
                 number = int(board[i][j])
                 box_of_interest = boxes[box_mappings[(i, j)]]
-                if number in rows[i]:
-                    print(f"number {number} in row {i+1}")
-                    print(f"at index ({i},{j})")
-                    print(rows[i])
-                    return False
-
-                if number in columns[j]:
-                    print(f"number {number} in column {i+1}")
-                    return False
-
-                if number in box_of_interest:
-                    print(f"number {number} in box {box_mappings[(i,j)]}")
+                if (
+                    number in rows[i]
+                    or number in columns[j]
+                    or number in box_of_interest
+                ):
                     return False
                 rows[i].add(number)
                 columns[j].add(number)
                 box_of_interest.add(number)
-
         return True
 
 
